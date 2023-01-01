b1288e7ccc33bc5741ecaf3b8a22685a34a5a3a6
Gal Siton <galsit@post.bgu.ac.il>
2023-01-01 13:32:52 +0200

    update

--- my_py_pkg/my_py_pkg/acceleration_tune_node.py
+++ my_py_pkg/my_py_pkg/acceleration_tune_node.py
@@ -6,8 +6,6 @@ import rclpy
 from rclpy.node import Node
 import numpy as np
 from mavsdk.offboard import (Attitude, OffboardError)
-import rospy
-from sensor_msgs.msg import Imu
 
 
 
@@ -27,7 +25,7 @@ async def run():
 
    
     # Start the tasks
-    #asyncio.ensure_future(print_imu(drone))
+    asyncio.ensure_future(print_imu(drone))
     
     
     print("-- Setting initial setpoint")
@@ -45,23 +43,23 @@ async def run():
 
     print("-- Go up at 70% thrust")
     await drone.offboard.set_attitude(Attitude(0.0, 0.0, 0.0, 0.7))
-    #print_imu(drone)
+    print_imu(drone)
     await asyncio.sleep(20)
     
 
     print("-- Roll 30 at 60% thrust")
     await drone.offboard.set_attitude(Attitude(10.0, 0.0, 0.0, 0.6))
-    #print_imu(drone)
+    print_imu(drone)
     await asyncio.sleep(20)
 
     print("-- Roll -30 at 60% thrust")
     await drone.offboard.set_attitude(Attitude(-10.0, 0.0, 0.0, 0.6))
-    #print_imu(drone)
+    print_imu(drone)
     await asyncio.sleep(20)
 
     print("-- Hover at 60% thrust")
     await drone.offboard.set_attitude(Attitude(0.0, 0.0, 0.0, 0.6))
-    #print_imu(drone)
+    print_imu(drone)
     await asyncio.sleep(20)
 
     print("-- Stopping offboard")
@@ -74,32 +72,17 @@ async def run():
     await drone.action.land()
 
 
-#async def print_imu(drone):
-    #async for imu in drone.telemetry.subscribe_scaled_imu(): 
-        #acceleration_x = imu.acceleration_frd.forward_m_s2
-        #acceleration_y = imu.acceleration_frd.right_m_s2
-        #acceleration_z = imu.acceleration_frd.down_m_s2
-        #angular_velocity__x = imu.angular_velocity_frd.forward_rad_s
-        #angular_velocity_y = imu.angular_velocity_frd.right_rad_s
-        #angular_velocity_z = imu.angular_velocity_frd.down_rad_s
-        #imu_matrix = np.c_[acceleration_x, acceleration_y, acceleration_z, angular_velocity__x, angular_velocity_y,angular_velocity_z]
+async def print_imu(drone):
+    async for imu in drone.telemetry.imu(): 
+        acceleration_x = imu.acceleration_frd.forward_m_s2
+        acceleration_y = imu.acceleration_frd.right_m_s2
+        acceleration_z = imu.acceleration_frd.down_m_s2
+        angular_velocity__x = imu.angular_velocity_frd.forward_rad_s
+        angular_velocity_y = imu.angular_velocity_frd.right_rad_s
+        angular_velocity_z = imu.angular_velocity_frd.down_rad_s
+        imu_matrix = np.c_[acceleration_x, acceleration_y, acceleration_z, angular_velocity__x, angular_velocity_y,angular_velocity_z]
        
-        #print(imu_matrix)
- # Initialize the node
-rospy.init_node("scaled_imu_node")
-
-def scaled_imu_callback(msg):
-    # This function will be called every time the scaled IMU data is updated
-    print(f"Acceleration: {msg.linear_acceleration}")
-    print(f"Angular velocity: {msg.angular_velocity}")
-    print(f"Quaternion: {msg.orientation}")
-
-# Subscribe to the scaled_imu topic
-sub = rospy.Subscriber("scaled_imu", Imu, scaled_imu_callback)
-
-# Spin to keep the node alive
-rospy.spin()       
-
+        print(imu_matrix)
 
 if __name__ == "__main__":
     # Start the main function
